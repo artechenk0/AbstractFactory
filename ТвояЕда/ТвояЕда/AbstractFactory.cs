6485011c15517a17df845cc329b6b077d099e1b5
Артем Боченин <67646834+artechenk0@users.noreply.github.com>
2022-04-04 01:17:54 +0300

    Add files via upload

---
+++ ТвояЕда/ТвояЕда/AbstractFactory.cs
@@ -0,0 +1,69 @@
+﻿namespace ТвояЕда
+{
+    public abstract class Burger
+    {
+        public abstract string CreateProductA();
+        public abstract string CreateProductB();
+        public abstract string CreateProductC();
+        public abstract string CreateProductD();
+        public abstract string CreateProductE();
+    }
+    public class SqrHumburger : Burger
+    {
+        public override string CreateProductA() => new SqrBulochka().Name;
+        public override string CreateProductB() => new ChickencCutlet().Name;
+        public override string CreateProductC() => new Cheese().Name;
+        public override string CreateProductD() => new GarlicSauce().Name;
+        public override string CreateProductE() => new Cucumbers().Name;
+    }
+    public class RoundHumburger : Burger
+    {
+        public override string CreateProductA() => new RoundBulochka().Name;
+        public override string CreateProductB() => new RoundBulochka().Name;
+        public override string CreateProductC() => new Cheese().Name;
+        public override string CreateProductD() => new CheeseSauce().Name;
+        public override string CreateProductE() => new Tomatoes().Name;
+    }
+    public class BigMac : Burger
+    {
+        public override string CreateProductA() => new RoundBulochka().Name;
+        public override string CreateProductB() => new BeefCutlet().Name;
+        public override string CreateProductC() => new Onion().Name;
+        public override string CreateProductD() => new Bacon().Name;
+        public override string CreateProductE() => new Cucumbers().Name;
+    }
+    public class TexasBurger : Burger
+    {
+        public override string CreateProductA() => new SqrBulochka().Name;
+        public override string CreateProductB() => new BeefCutlet().Name;
+        public override string CreateProductC() => new Cheese().Name;
+        public override string CreateProductD() => new CheeseSauce().Name;
+        public override string CreateProductE() => new Tomatoes().Name;
+    }
+    abstract class Bulochka { public abstract string Name { get; } }
+    class SqrBulochka : Bulochka { public override string Name => "Квадратная булочка"; }
+    class RoundBulochka : Bulochka { public override string Name => "Круглая булочка"; }
+
+    abstract class Sauce { public abstract string Name { get; } }
+    class GarlicSauce : Sauce { public override string Name => "Чесночный соус"; }
+    class CheeseSauce : Sauce { public override string Name => "Сырный соус"; }
+
+    abstract class Vegetables { public abstract string Name { get; } }
+    class Cucumbers : Vegetables { public override string Name => "Огурцы"; }
+    class Tomatoes : Vegetables { public override string Name => "Помидоры"; }
+    class Onion : Vegetables { public override string Name => "Лук репчатый"; }
+
+    abstract class Cutlet { public abstract string Name { get; } }
+    class ChickencCutlet : Cutlet { public override string Name => "Куриная котлета"; }
+    class BeefCutlet : Cutlet { public override string Name => "Говяжья котлета"; }
+
+    abstract class Additionally { public abstract string Name { get; } }
+    class Cheese : Additionally { public override string Name => "Сыр"; }
+    class Bacon : Additionally { public override string Name => "Бекон (нарезка)"; }
+
+
+
+
+
+}
+
---
+++ ТвояЕда/ТвояЕда/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ТвояЕда
+{
+    public partial class Form1 : Form
+    {
+        public List<string> products = new List<string>();
+        public void ClientMethod(Burger factory)
+        {
+            var productA = factory.CreateProductA();
+            var productB = factory.CreateProductB();
+            var productC = factory.CreateProductC();
+            var productD = factory.CreateProductD();
+            var productE = factory.CreateProductE();
+
+            products.Add(productA);
+            products.Add(productB);
+            products.Add(productC);
+            products.Add(productD);
+            products.Add(productE); 
+        }
+        public void ClientMain(Burger factory)
+        {
+            textBox1.Clear();
+            products.Clear();
+            ClientMethod(factory);
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                textBox1.Text += Convert.ToString(products[i]) + Environment.NewLine;
+            }
+        }
+        public Form1() => InitializeComponent();
+
+        private void button1_Click(object sender, EventArgs e) => ClientMain(new SqrHumburger());
+        private void button2_Click(object sender, EventArgs e) => ClientMain(new RoundHumburger());
+        private void button3_Click(object sender, EventArgs e) => ClientMain(new BigMac());
+        private void button4_Click(object sender, EventArgs e) => ClientMain(new TexasBurger());
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Text = "Приятного вам аппетита!";
+        }
+    }
+}
